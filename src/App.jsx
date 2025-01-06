1aa78ab6a30d18ecb39d5793a3f89ac857a8ba37
Bawantha <bawanthapasqual@gmail.com>
2025-01-06 17:15:56 +0530

    remove ani cur in mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,28 @@ import { Routes, Route } from "react-router-dom";
 // import { Analytics } from "@vercel/analytics/react";
 import Home from "./components/pages/Home";
 import AnimatedCursor from "react-animated-cursor";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768); 
+    };
+
+    checkMobile();
+
+    window.addEventListener("resize", checkMobile);
+
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
   return (
     <>
-      <AnimatedCursor />
+      {!isMobile && <AnimatedCursor />} 
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
